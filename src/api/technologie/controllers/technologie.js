0a32055ad2899eef417cceb6c9e2e952428e9e08
oxypomme <oxytom8@gmail.com>
2022-02-02 08:40:23 +0100

    feat: :sparkles: Added techs models

---
+++ src/api/technologie/controllers/technologie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  technologie controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::technologie.technologie');
---
+++ src/api/technologie/services/technologie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * technologie service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::technologie.technologie');
