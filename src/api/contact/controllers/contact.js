d979ca9152cba0924b504cfa139b941af826b159
oxypomme <oxytom8@gmail.com>
2022-03-08 14:27:50 +0100

    fix: :lock: Prevented XSS

--- src/api/contact/controllers/contact.js
+++ src/api/contact/controllers/contact.js
@@ -1,11 +1,21 @@
+const { encode } = require("html-entities");
+
 const emailTemplate = {
   subject: "Demande de contact : <%= name %>",
-  text: `<%= name %> (<%= email %>) vous a envoyé un message via.`,
-  html: `<h4><a href="mailto:<%= email %>"><%= name %></a> vous a envoyé un message :</h4>
+  text: `<%= name %> (<%= email %>) vous a envoyé un message via votre Portflio.`,
+  html: `<h4><a href="mailto:<%= email %>"><%= name %></a> vous a envoyé un message via votre Portflio :</h4>
+    <br />
 		<p><%= text %><p>
-		<p style="color: grey; font-style: italic;">
+    <br />
+		<p style="font-style: italic;">
 			Vous pouvez lui répondre en envoyant un mail à <a href="mailto:<%= email %>"><%= email %></a>
 			ou en répondant à ce message.
+		</p>
+    <br/>
+    <p style="color: grey; font-style: italic;">
+      ----</br >
+			Ceci est un message automatisé, merci de ne pas répondre à ce mail (sauf indication contraire).<br/>
+      Pour toute réclamation, envoyez un mail à <a href="mailto:tom.sublet@oxypomme.fr">tom.sublet@oxypomme.fr</a>
 		</p>`,
 };
 
@@ -19,7 +29,7 @@ module.exports = {
     };
     const content = {
       name,
-      text,
+      text: encode(text),
       email,
     };
 
